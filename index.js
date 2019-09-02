16493ee1be8a670f2b6663f031dc23d27ef3c3a3
marcus chukwuoma <marcus2cu@gmail.com>
2019-09-02 12:56:06 +0100

    implement data caching

--- index.js
+++ index.js
@@ -33,7 +33,22 @@ const getRepos = async (req, res, next) => {
   }
 }
 
-app.get('/repos/:username', getRepos);
+// cache middleware
+const cache = (req, res, next) => {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  })
+}
+
+app.get('/repos/:username', cache, getRepos);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
